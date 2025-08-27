81f2032dcc79f39d2381446b4a7425cf28c1d406
Puspendu <puspenduofficial@gmail.com>
2025-08-27 11:51:26 +0530

    Update server.sh

--- deployment/server.sh
+++ deployment/server.sh
@@ -2,9 +2,9 @@
 docker stop api_predict
 docker rm api_predict
 docker rmi api_predict
-rm -rf api_predict
-git clone git@github.com:Mukeshrinwa/api_predict.git
-cd api_predict
+rm -rf predict
+git clone git@github.com:puspendudas/predict.git
+cd predict
 docker build --no-cache -t api_predict .
 docker run -d --name api_predict -p 8080:8080 api_predict
 cd ..
