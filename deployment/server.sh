ba5807a26d50125c2915350e260c6f79401f919e
Puspendu <puspenduofficial@gmail.com>
2025-12-30 13:05:58 +0530

    Clone predict repo into Server directory

--- deployment/server.sh
+++ deployment/server.sh
@@ -1,9 +1,8 @@
-
 docker stop api_predict
 docker rm api_predict
 docker rmi api_predict
 rm -rf Server
-git clone git@github.com:puspendudas/predict.git
+git clone git@github.com:puspendudas/predict.git Server
 cd Server
 docker build --no-cache -t api_predict .
 docker run -d --name api_predict -p 7000:7000 api_predict
