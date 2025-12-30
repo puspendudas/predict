ec95d5f974525f4ae1e149116241513bfd229c94
Puspendu <puspenduofficial@gmail.com>
2025-12-30 13:04:15 +0530

    Update server script to use 'Server' directory

--- deployment/server.sh
+++ deployment/server.sh
@@ -2,9 +2,9 @@
 docker stop api_predict
 docker rm api_predict
 docker rmi api_predict
-rm -rf predict
+rm -rf Server
 git clone git@github.com:puspendudas/predict.git
-cd predict
+cd Server
 docker build --no-cache -t api_predict .
 docker run -d --name api_predict -p 7000:7000 api_predict
 cd ..
